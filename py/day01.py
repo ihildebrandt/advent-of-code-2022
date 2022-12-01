3ce573eed7b8bf95dbb5fd91fa817bf8d60a6047
P. Isaac Hildebrandt <isaach@phase2online.com>
2022-12-01 07:38:04 -0600

    include python solutions

---
+++ py/day01.py
@@ -0,0 +1,21 @@
+from functools import reduce
+from operator import add
+
+def main():
+    f = open("input/01/input.txt", "r")
+    
+    calories = [0]
+    calIdx = 0
+
+    for line in f.readlines():
+        if line.strip() == "":
+            calories.append(0)
+            calIdx += 1
+        else:
+            calories[calIdx] += int(line)
+
+    calories.sort(reverse=True)
+    print(reduce(add, calories[0:3]))
+
+if __name__ == "__main__":
+    main()
