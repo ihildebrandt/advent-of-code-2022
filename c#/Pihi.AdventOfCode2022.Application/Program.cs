2514ce1177e8a98f5e2d5ba8fa30a2be0468a64f
P. Isaac Hildebrandt <isaac@pihimedia.com>
2022-12-11 22:47:12 -0600

    upgrade to use run profiles

--- c#/Pihi.AdventOfCode2022.Application/Program.cs
+++ c#/Pihi.AdventOfCode2022.Application/Program.cs
@@ -1,7 +1,13 @@
-﻿using Pihi.AdventOfCode2022.Day11;
+﻿using Pihi.AdventOfCode2022.Common;
+using System.Reflection;
 
-var day = 11;
-var input = "input";
+var input = args[1];
+var day = int.Parse(args[0]);
+
+var assembly = Assembly.LoadFrom($"./Pihi.AdventOfCode2022.Day{day:00}.dll");
+var type = assembly.GetExportedTypes().Single(t => t.IsAssignableTo(typeof(RunnerBase)));
+var constructor = type.GetConstructor(new Type[] { typeof(Stream) });
 
 using var inputStream = File.OpenRead($"input/{day:00}/{input}.txt");
-new Runner(inputStream).Run();
+var runner = (RunnerBase)(constructor.Invoke(new[] { inputStream }));
+runner.Run();
